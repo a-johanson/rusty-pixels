76f75495481724e131e0cc1c693f16cbbe49371f
arne-johanson <arne.johanson@xing.com>
2021-12-06 22:41:09 +0100

    put pixels with blending

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
 use show_image::{Color, WindowOptions, event, create_window};
 use image::{ImageFormat, RgbImage};
 
+/// r, g, b in [0, 255]
+/// alpha in [0.0, 1.0]
+fn put_pixel(image: &mut RgbImage, x: i32, y: i32, r: u8, g: u8, b: u8, alpha: f32) {
+    if x >= 0 && y >= 0 && (x as u32) < image.dimensions().0 && (y as u32) < image.dimensions().1 {
+        let pixel = image.get_pixel_mut(x as u32, y as u32);
+        let data = (*pixel as image::Rgb<u8>).0;
+
+        let bg_a = 1.0 - alpha;
+        let bg_r = data[0] as f32 * bg_a;
+        let bg_g = data[1] as f32 * bg_a;
+        let bg_b = data[2] as f32 * bg_a;
+
+        *pixel = image::Rgb([
+            (r as f32 * alpha + bg_r) as u8,
+            (g as f32 * alpha + bg_g) as u8,
+            (b as f32 * alpha + bg_b) as u8
+        ]);
+    }
+}
+
 #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let image = RgbImage::from_fn(100, 100, |x, y| {
-        if (x + y) % 5 == 0 {
-            image::Rgb([0u8, 0u8, 0u8])
-        } else {
-            image::Rgb([255u8, 255u8, 255u8])
-        }
+    const WIDTH: i32  = 100;
+    const HEIGHT: i32 = 100;
+
+    let mut image = RgbImage::from_fn(WIDTH as u32, HEIGHT as u32, |_x, _y| {
+        image::Rgb([35u8, 34u8, 28u8])
     });
+
+    for x in 0..WIDTH {
+        put_pixel(&mut image, x, HEIGHT / 2, 242, 238, 230, 0.25);
+        put_pixel(&mut image, x + 1, HEIGHT / 2, 242, 238, 230, 0.25);
+        put_pixel(&mut image, x + 2, HEIGHT / 2, 242, 238, 230, 0.25);
+        put_pixel(&mut image, x + 3, HEIGHT / 2, 242, 238, 230, 0.25);
+        put_pixel(&mut image, x, HEIGHT / 2 + 1, 242, 238, 230, 1.0);
+    }
 
     // image.save_with_format("image.png", ImageFormat::Png)?;
 
